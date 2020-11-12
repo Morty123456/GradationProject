39242fa754d5724f5923cc7f14dedc56a4944973
wzh <13021151355@163.com>
2020-11-12 21:35:50 +0800

    写注释

--- HMM/hmm.py
+++ HMM/hmm.py
@@ -2,13 +2,18 @@ from hmmlearn import hmm
 import numpy as np
 from matplotlib import pyplot as plt
 import pandas as pd
+# 6个隐藏状态
 n = 6
 CSV_PATH = './1.csv'
 data = pd.read_csv(CSV_PATH, index_col=0)
-# print(data)
+
+# 成交量
 volume = data['volume']
+
+# 成交金额
 close = data['close']
 
+# np.array 创建数组， np.log 计算自然对数， np.diff 计算差值 a[n]-a[n-1]
 logDel = np.log(np.array(data['high'])) - np.log(np.array(data['low']))
 logRet_1 = np.array(np.diff(np.log(close)))
 logRet_5 = np.log(np.array(close[5:])) - np.log(np.array(close[:-5]))
@@ -30,7 +35,7 @@ for i in range(n):
     pos = (hidden_states == i)
     plt.plot_date(Date[pos], close[pos], 'o', label='hidden state %d' % i, lw=2)
     plt.legend()
-plt.show()
+# plt.show()
 
 res = pd.DataFrame({'Date': Date, 'logReg_1': logRet_1, 'state': hidden_states}).set_index('Date')
 series = res.logReg_1
@@ -45,7 +50,7 @@ for i in range(n):
     templist.append(data_i[-1])
     plt.plot_date(Date, data_i, '-', label='hidden state %d' % i)
     plt.legend()
-plt.show()
+# plt.show()
 
 templist = np.array(templist).argsort()
 long = (hidden_states == templist[-1]) + (hidden_states == templist[-2])  # 买入
@@ -56,4 +61,4 @@ short = np.append(0, short[:-1])
 plt.figure(figsize=(25, 18))
 res['ret'] = series.multiply(long) - series.multiply(short)
 plt.plot_date(Date, np.exp(res['ret'].cumsum()), 'r-')
-plt.show()
+# plt.show()
