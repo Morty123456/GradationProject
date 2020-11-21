fe2c229dd195d7a30f000f28a442fe1aabea284b
wzh <13021151355@163.com>
2020-11-21 20:49:36 +0800

    fft完善

--- FFT/fft.py
+++ FFT/fft.py
@@ -16,8 +16,6 @@ y = 7*np.sin(2*np.pi*200*x) + 5*np.sin(2*np.pi*400*x) + 3*np.cos(2*np.pi*600*x)
 # fft的 振幅谱 和 相位谱,是 通过对傅里叶变换得到的复数结果，进行进一步计算得到的
 # 复数的模(绝对值) 就是对应的 "振幅谱",复数所对应的角度 就是所对应的 "相位谱"
 fft_y = fft(y)
-print(len(fft_y))
-print(fft_y)
 
 # 采样频率(每分钟采样数)
 N = 1400
@@ -27,14 +25,30 @@ abs_y = np.abs(fft_y)   # 取复数的绝对值(振幅谱)
 angle_y = np.angle(fft_y)   # 取复数的角度(相位谱)
 normalization_y = abs_y/N   # 归一化处理
 normalization_half_y = normalization_y[range(int(N/2))]    # 取一半的区间
+
 # print(len(normalization_half_y))
-# print(normalization_half_y)
+res = {}
+resLocl = 0
+i = 0
+while(i < len(normalization_half_y)):
+    if(normalization_half_y[i]>0.1):
+        j = i
+        while( j<len(normalization_half_y) and normalization_half_y[j]>0.1 ):
+            j += 1
+        res[resLocl] = (i+j)/2
+        resLocl += 1
+        i = j
+    i += 1
+
+for r in res:
+    print(res[r])
+
 plt.figure()
 plt.plot(x[0:50], y[0:50])
-plt.show()
+# plt.show()
 
 plt.figure()
 # plt.plot(half_x[range(int(5))], normalization_half_y[range(int(5))], 'blue')
 plt.plot(half_x, normalization_half_y, 'blue')
 plt.title('单边频谱(归一化)', fontsize=9, color='blue')
-plt.show()
+# plt.show()
